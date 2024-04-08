c17f2bb514b11cd44130cccc6fafa95a424472be
Amir202000160 <amirsamir225@gmail.com>
2024-04-08 22:42:08 +0200

    UI Canvas
    UI for our 2d game SunnyLand

---
+++ Sunny Land/Assets/Scripts/Core/EndButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndBuuton : MonoBehaviour
+{
+public void EndGame()
+    { 
+        Application.Quit();
+    }
+}
